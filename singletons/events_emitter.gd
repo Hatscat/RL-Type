8328b9ad3c0914006e664a47678b985e186c4a83
Maxime Lo Re <solfen@hotmail.com>
2016-04-12 23:12:37 +0200

    portals

--- singletons/events_emitter.gd
+++ singletons/events_emitter.gd
@@ -2,5 +2,6 @@ extends Node
 
 signal score_changed(score)
 signal player_hit(new_life)
+signal portal_entered()
 
 
--- singletons/game_data.gd
+++ singletons/game_data.gd
@@ -2,6 +2,6 @@ extends Node
 
 var score = 0
 
-func set_score(new_score):
-	score = new_score
+func add_score(new_score):
+	score += new_score
 	get_node("/root/events_emitter").emit_signal("score_changed", score)
